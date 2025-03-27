9116ad3e347871c3216ca7f1c6ef3ac8635a3aad
dadoos10 <dadoos.dc@gmail.com>
2025-03-28 00:01:47 +0200

    Updated Python script

---
+++ hello.py
@@ -0,0 +1,13 @@
+print("hi")
+# Rona's Project:
+# In this project we research the linearity relationship between biological components to quantitative MRI (qMRI).
+# it is well known (source!) that lypid and iron represent linear connection to qMRI parameters, seperatly. 
+# Here we want to ask if the combination of the two types represent linearity as well.
+# the players: 
+# biological components: lipids: PC,PC_SphingoMyalin.....
+# qMRI parameters: R1,R2s,MT,MTV.  
+# import pandas as pd
+# import NumPy as np
+# first we want to crate a scan rescan plot, to see the stability of our experimants.
+# Actualy we make two plots, one for iron, one for lipyd, since they behave different.
+                 
