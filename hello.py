4c6af5e0373792a9aedb7e3b636f69bc286bb332
dadoos10 <dadoos.dc@gmail.com>
2025-03-30 14:42:28 +0300

    30.3

--- hello.py
+++ hello.py
@@ -1,4 +1,3 @@
-print("hi")
 # Rona's Project:
 # In this project we research the linearity relationship between biological components to quantitative MRI (qMRI).
 # it is well known (source!) that lypid and iron represent linear connection to qMRI parameters, seperatly. 
@@ -15,6 +14,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+from toolBox import *
 
 def extract_zero_com_exp(exp = 1, data = None, lipid = False):
     """
@@ -29,25 +29,55 @@ def extract_zero_com_exp(exp = 1, data = None, lipid = False):
     else:
         return data[(data['ExpNum'] == exp) & (data['Lipid (fraction)'] == 0)]
 
-# # Read the data file into a pandas dataframe
-data = pd.read_excel('data.xlsx', sheet_name=0)
-exp = 1
-data_1 =  extract_zero_com_exp(exp, data = data, lipid = True)
-exp = 12
-data_12 =  extract_zero_com_exp(exp, data = data, lipid = True)
-print(data_1)
-print(data_12)
-# delete duplicates of Lipid fraction values, and keep the first one.
-data_1 = data_1.drop_duplicates(subset=['Lipid (fraction)'], keep='first')
-data_12 = data_12.drop_duplicates(subset=['Lipid (fraction)'], keep='first')
-# # Create a scatter plot for the data, the x axis is the R1 (1/sec) of data_1, and the y axis is the R1 (1/sec) of data_12.
-# # The title of the plot is "R1 (1/sec) of data_1 vs. R1 (1/sec) of data_12"
-# # The x axis label is "R1 (1/sec) of data_1" and the y axis label is "R1 (1/sec) of data_12".
-plt.figure(figsize=(10, 6))
-plt.scatter(data_1['R1 (1/sec)'], data_12['R1 (1/sec)'], alpha=0.5)
-plt.title('R1 (1/sec) of data_1 vs. R1 (1/sec) of data_12')
-plt.xlabel('R1 (1/sec) of data_1')
-plt.ylabel('R1 (1/sec) of data_12')
-plt.grid(True)
-plt.show()
+def handle_duplicates_and_sort(data_1, data_2):
+    """
+    This function handles duplicates and sorts the data by Lipid fraction values.
+    :return: None
+    """
+    print(f"data_1: \n{data_1}.\n data_2: \n{data_2}")
+    # delete duplicates of Lipid fraction values, and keep the first one.
+    data_1 = data_1.drop_duplicates(subset=['Lipid (fraction)'], keep='first')
+    data_2 = data_2.drop_duplicates(subset=['Lipid (fraction)'], keep='first')
+    # order datasets by Lipid fraction values.
+    data_1 = data_1.sort_values(by=['Lipid (fraction)'])
+    data_2 = data_2.sort_values(by=['Lipid (fraction)'])
+    return data_1, data_2
+
+def plot_linearity(data_1, data_2, exp1, exp2, mri_param = 'R1 (1/sec)',lipid = True):
+    to_show = {True: 'Lipid type', False: 'Iron type'}
+    # # Create a scatter plot for the data, the x axis is the R1 (1/sec) of data_1, and the y axis is the R1 (1/sec) of data_12.
+    # # The title of the plot is "R1 (1/sec) of data_1 vs. R1 (1/sec) of data_12"
+    # # The x axis label is "R1 (1/sec) of data_1" and the y axis label is "R1 (1/sec) of data_12".
+    plt.figure(figsize=(10, 6))
+    plt.scatter(data_1[mri_param], data_2[mri_param], alpha=0.5)
+    plt.title(f'{mri_param} of {exp1} vs. {mri_param} of {exp2} - for {data_1[to_show[lipid]].iloc[0]}')
+    plt.xlabel(f'{mri_param} of {exp1}')
+    plt.ylabel(f'{mri_param} of {exp2}')
+    plt.grid(True)
+    plt.show()
+
+def run_test_retest(data,exps_pair,MRI_param = 'R1 (1/sec)',lipid = True):
+    data_1 =  extract_zero_com_exp(exps_pair[0], data, lipid)
+    data_2 =  extract_zero_com_exp(exps_pair[1], data, lipid)
+    data_1,data_2 = handle_duplicates_and_sort(data_1, data_2)
+    plot_linearity(data_1, data_2, exps_pair[0], exps_pair[1],MRI_param,lipid)
+
+
+# main function
+if __name__ == "__main__":
+    print("d")
+    # # Read the data file into a pandas dataframe
+    data = pd.read_excel('data.xlsx', sheet_name=0)
+    # exp_lipid_pairs_to_check = [PC_Cholest_pair,PC_SM_pair,PC_pair]
+    # for lipid_pair in exp_lipid_pairs_to_check:
+    #     run_test_retest(data,lipid_pair)
+
+    exp_iron_pairs_to_check = [Fe2_pair, Fe3_pair, Ferittin_pair, Tranferrin_pair]
+    for iron_pair in exp_iron_pairs_to_check:
+        run_test_retest(data,iron_pair, lipid = False)
+
+
+
+    
+
 
---
+++ toolBox.py
@@ -0,0 +1,12 @@
+#lipid pairs to check
+PC_Cholest_pair = (1,12)
+PC_SM_pair = (3,4)
+PC_pair = (2,5)
+
+# iron pairs to check
+Fe2_pair = (1,2)
+Fe3_pair = (11,12)
+Ferittin_pair = (4,5)
+Tranferrin_pair = (7,9)
+
+
