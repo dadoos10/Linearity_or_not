54ac076ecdaa3e661209f1505969c610442762c5
dadoos10 <dadoos.dc@gmail.com>
2025-03-31 13:16:57 +0300

    31/3

--- hello.py
+++ hello.py
@@ -15,6 +15,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 from toolBox import *
+import read_xlsx
 
 def extract_zero_com_exp(exp = 1, data = None, lipid = False):
     """
@@ -29,18 +30,38 @@ def extract_zero_com_exp(exp = 1, data = None, lipid = False):
     else:
         return data[(data['ExpNum'] == exp) & (data['Lipid (fraction)'] == 0)]
 
-def handle_duplicates_and_sort(data_1, data_2):
+def handle_duplicates_and_sort(data_1, data_2, lipid=True):
     """
-    This function handles duplicates and sorts the data by Lipid fraction values.
-    :return: None
+    Handles duplicates and sorts data by Lipid fraction or Fe values.
+    
+    :param data_1: First DataFrame
+    :param data_2: Second DataFrame
+    :param lipid: Boolean flag to choose column (True for Lipid fraction, False for Fe)
+    :return: Processed data_1 and data_2
     """
-    print(f"data_1: \n{data_1}.\n data_2: \n{data_2}")
-    # delete duplicates of Lipid fraction values, and keep the first one.
-    data_1 = data_1.drop_duplicates(subset=['Lipid (fraction)'], keep='first')
-    data_2 = data_2.drop_duplicates(subset=['Lipid (fraction)'], keep='first')
-    # order datasets by Lipid fraction values.
-    data_1 = data_1.sort_values(by=['Lipid (fraction)'])
-    data_2 = data_2.sort_values(by=['Lipid (fraction)'])
+    # Print first 6 columns of data_1 and data_2
+    print(f"data_1: \n{data_1.iloc[:, :6]}.\n data_2: \n{data_2.iloc[:, :6]}")
+
+    subset = {True: ['Lipid (fraction)'], False: ['[Fe] (mg/ml)']}
+    column_name = subset[lipid][0]  # Extract the correct column name
+
+    # Drop duplicates based on the selected column
+    data_1 = data_1.drop_duplicates(subset=column_name, keep='first')
+    data_2 = data_2.drop_duplicates(subset=column_name, keep='first')
+
+    # Ensure both datasets have rows in common based on the selected column
+    common_values = pd.merge(data_1, data_2, on=column_name, how='inner')[column_name]
+
+    # Filter to keep only common values
+    data_1 = data_1[data_1[column_name].isin(common_values)]
+    data_2 = data_2[data_2[column_name].isin(common_values)]
+
+    print(f"Filtered data_1: \n{data_1}.\nFiltered data_2: \n{data_2}")
+
+    # Sort datasets by the selected column
+    data_1 = data_1.sort_values(by=column_name)
+    data_2 = data_2.sort_values(by=column_name)
+
     return data_1, data_2
 
 def plot_linearity(data_1, data_2, exp1, exp2, mri_param = 'R1 (1/sec)',lipid = True):
@@ -59,16 +80,15 @@ def plot_linearity(data_1, data_2, exp1, exp2, mri_param = 'R1 (1/sec)',lipid = 
 def run_test_retest(data,exps_pair,MRI_param = 'R1 (1/sec)',lipid = True):
     data_1 =  extract_zero_com_exp(exps_pair[0], data, lipid)
     data_2 =  extract_zero_com_exp(exps_pair[1], data, lipid)
-    data_1,data_2 = handle_duplicates_and_sort(data_1, data_2)
+    data_1,data_2 = handle_duplicates_and_sort(data_1, data_2,lipid)
     plot_linearity(data_1, data_2, exps_pair[0], exps_pair[1],MRI_param,lipid)
 
 
 # main function
 if __name__ == "__main__":
-    print("d")
     # # Read the data file into a pandas dataframe
     data = pd.read_excel('data.xlsx', sheet_name=0)
-    # exp_lipid_pairs_to_check = [PC_Cholest_pair,PC_SM_pair,PC_pair]
+    exp_lipid_pairs_to_check = [PC_Cholest_pair,PC_SM_pair,PC_pair]
     # for lipid_pair in exp_lipid_pairs_to_check:
     #     run_test_retest(data,lipid_pair)
 
